40dc88545c304dd1b3a7a7e83f42f33ab7d2a36f
Joris Gillis <joris.gillis@mech.kuleuven.be>
2011-10-05 18:09:08 +0200

    Temptative bashrc

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 sudo apt-get -y install aptitude
 sudo aptitude update
+
+./myreps.sh
+registerbashrc bashrc
+
 ./general.sh
 ./development.sh
 ./scientific.sh
--- myreps.sh
+++ myreps.sh
@@ -1,4 +1,33 @@
-echo "Please enter your u number"
-read unum
-git clone $unum@git.mech.kuleuven.be:gitroot/phd.git
-git clone $unum@git.mech.kuleuven.be:gitroot/music.git
+#!/bin/bash
+echo  -n "Would you like to have repositories available? (Y/N)"
+read ANSWER
+if [ "$ANSWER" = "Y" ]; then
+
+  mkdir -p .ssh
+  cd ~/.ssh
+  echo -n "What is your e-mail? "
+
+  echo "" | ssh-keygen -t rsa -C $email
+
+  cat id_rsa.pub
+  sudo aptitude -y install xclip
+  xclip id_rsa.pub
+
+  echo "Your public key has been generated and put on the clipboard."
+  echo "Navigate to https://github.com/account/ssh and paste the key there."
+  echo "Press any key when you finished this action."
+  read dummy
+
+  cd ~
+  mkdir -p tools
+  cd tools && git clone git@github.com:jgillis/Utilities.git utilities
+  
+
+else
+  cd ~
+  mkdir -p tools
+  wget https://github.com/jgillis/Utilities/tarball/master && tar -xvf master && mv jgillis-Utilities*  tools/utilities
+
+fi
+
+cd ~/tools/utilities && ./install.sh
