ac47a5c0f8b48ef78229a4b0a606fce646515871
Joris Gillis <joris.gillis@mech.kuleuven.be>
2011-10-05 16:36:28 +0200

    Adding bootstrap script.

---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~
+echo "This is clean-install bootstrap"
+wget https://github.com/jgillis/Clean-install/tarball/master && tar -xvf master && mv jgillis-Clean-install*  clean-install && cd clean-install && sudo sh ./install.sh
