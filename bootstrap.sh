737599947a41bfd2159765e3687844aa80cfae5b
Joris Gillis <joris.gillis@mech.kuleuven.be>
2011-10-05 18:13:18 +0200

    remove clean-install first if already present

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd ~
 echo "This is clean-install bootstrap"
-wget https://github.com/jgillis/Clean-install/tarball/master && tar -xvf master && mv -f jgillis-Clean-install*  clean-install && cd clean-install && sudo sh ./install.sh
+rm -rf clean-install
+wget https://github.com/jgillis/Clean-install/tarball/master && tar -xvf master && mv jgillis-Clean-install*  clean-install && cd clean-install && sudo sh ./install.sh
 cd ~
 rm bootstrap.sh
 rm master
