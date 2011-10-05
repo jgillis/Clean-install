2d5829f152822eb398f6556dd30af0fde070d6c0
Joris Gillis <joris.gillis@mech.kuleuven.be>
2011-10-05 16:41:57 +0200

    Added post-bootstrap cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,6 @@
 cd ~
 echo "This is clean-install bootstrap"
 wget https://github.com/jgillis/Clean-install/tarball/master && tar -xvf master && mv jgillis-Clean-install*  clean-install && cd clean-install && sudo sh ./install.sh
+cd ~
+rm bootstrap.sh
+rm master
