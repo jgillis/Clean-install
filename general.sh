ca9b81f2a15ec33d498407d5d628b1d2cfc486af
JG <joris.gillis@mech.kuleuven.be>
2011-10-06 11:48:59 +0200

    Make sure that gitg and subversion are installed quickly. This allows to start coding immediately on the  vanilla system.

--- general.sh
+++ general.sh
@@ -1,2 +1,5 @@
-
-sudo aptitude -y install nautilus-open-terminal gstm subversion gitg emacs vim gimp winpdb doxygen meld nemiver samba ipe inkscape privoxy geogebra geogebra-gnome umlet kile scite kate openssh-server compizconfig-settings-manager dia pgf
+sudo aptitude install nautilus-open-terminal
+nautilus -q
+sudo aptitude -y install gitg
+sudo aptitude -y install subversion
+sudo aptitude -y install gstm emacs vim gimp winpdb doxygen meld nemiver samba ipe inkscape privoxy geogebra geogebra-gnome umlet kile scite kate openssh-server compizconfig-settings-manager dia pgf
