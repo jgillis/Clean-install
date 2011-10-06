52f6bf13e2fa7b41afbe99654a1f580241a5c898
JG <joris.gillis@mech.kuleuven.be>
2011-10-06 11:12:08 +0200

    More user messages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd ~
 echo "This is clean-install bootstrap"
+echo ""
+echo "At first stage, I will need some human guidance."
+echo "Please stand by for questions..."
+echo ""
+read -p "Presss any key to continue" DUMMY
+
 if [ -f clean-install ]; then
 rm clean-install
 fi
@@ -10,6 +16,7 @@ fi
 read -p "Would you like to have repositories available? (Y/N)" ANSWER
 if [ ! -f ~/.ssh/id_rsa.pub ]; then
 	read -p "What is your e-mail? " EMAIL
+	read -p "What is your name? " NAME
 	echo "" | ssh-keygen -t rsa -C $EMAIL
 fi
 
@@ -17,21 +24,17 @@ if [ "$ANSWER" = "Y" ]; then
 	mkdir -p ~/.ssh
 	cd ~/.ssh
 
-	sudo apt-get -y install xclip
-
 	if [ ! -f id_rsa.pub ]; then
 		echo ""
 		echo "************************************************************"
-		echo "Your public key has been generated and put on the clipboard."
+		echo "Your public key has been generated."
 		echo ""
 		echo "Navigate to https://github.com/account/ssh and paste the key there."
 	else
 		echo ""
 		echo "************************************************************"
-		echo "Your public key has been put on the clipboard."
 		echo "Navigate to https://github.com/account/ssh and paste the key there."
 	fi
-	cat id_rsa.pub | xclip
 	cat id_rsa.pub
 
 	echo "Press any key when you finished this action."
@@ -47,6 +50,8 @@ if [ "$ANSWER" = "Y" ]; then
 		echo "github.com" |  ssh-keyscan -f - -t rsa >> ~/.ssh/known_hosts
 	fi
 	sudo apt-get -y install git
+	git config --global user.name $NAME
+	git config --global user.email $EMAIL
 	cd ~/tools && git clone git@github.com:jgillis/Utilities.git utilities
 	cd ~ && git clone git@github.com:jgillis/Clean-install.git clean-install
 else
@@ -54,6 +59,8 @@ else
 	wget https://github.com/jgillis/Utilities/tarball/master && tar -xvf master && mv jgillis-Utilities*  ~/tools/utilities
 	cd ~ && wget https://github.com/jgillis/Clean-install/tarball/master && tar -xvf master && mv jgillis-Clean-install*  clean-install
 fi
+echo "Okay, your off the hook now. I will continue automatically"
+read -p "Presss any key to continue"  DUMMY
 cd ~/tools/utilities && ./install.sh
 cd ~/clean-install && sudo sh ./install.sh
 cd ~
