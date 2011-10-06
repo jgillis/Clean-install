f202ba3b8e7f9e8eadb0eaf266a2279233253ab7
JG <joris.gillis@mech.kuleuven.be>
2011-10-06 13:12:53 +0200

    Included octave headers

--- scientific.sh
+++ scientific.sh
@@ -1,4 +1,4 @@
 sudo apt-get install libsundials-*
 sudo aptitude -y install subversion cmake liblapack-dev coinor-libipopt-dev  mpich-bin libblas-dev libmumps-seq-4.9.2  gfortran python-dev python-numpy swig1.3 python-scipy python-visual libgtkglextmm-x11-1.2-dev python-matplotlib
 
-sudo aptitude -y install ipython wxmaxima octave spyder 
+sudo aptitude -y install ipython wxmaxima octave spyder octave3.2-headers
