a2def2134a4c5fae378a470b57022cf20aa9f1bf
JG <joris.gillis@mech.kuleuven.be>
2011-10-22 17:26:14 +0200

    ffmpeg is worthwile to have

--- general.sh
+++ general.sh
@@ -2,4 +2,4 @@ sudo aptitude install nautilus-open-terminal
 nautilus -q
 sudo aptitude -y install gitg
 sudo aptitude -y install subversion
-sudo aptitude -y install gstm emacs vim gimp winpdb doxygen meld nemiver samba ipe inkscape privoxy geogebra geogebra-gnome umlet kile scite kate openssh-server compizconfig-settings-manager dia pgf
+sudo aptitude -y install gstm emacs vim gimp winpdb doxygen meld nemiver samba ipe inkscape privoxy geogebra geogebra-gnome umlet kile scite kate openssh-server compizconfig-settings-manager dia pgf ffmpeg
