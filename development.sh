f5a2df1b0b25c539bd43ce3b2b1de81a7edaaa07
JG <joris.gillis@mech.kuleuven.be>
2011-10-23 00:42:27 +0200

    valgrind

--- development.sh
+++ development.sh
@@ -1,5 +1,5 @@
 
-sudo aptitude -y install automake cmake build-essential checkinstall apt-file
+sudo aptitude -y install automake cmake build-essential checkinstall apt-file valgrind
 
 # opencv deps
 sudo aptitude -y install libavcodec-dev libswscale-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libjasper-dev libavformat-dev libunicap2-dev libunicapgtk2 libxine-dev intltool
