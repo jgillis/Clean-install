fcd2b07f1022e599a8ad6b7e0239c0d39dc64810
JG <joris.gillis@mech.kuleuven.be>
2011-10-22 18:55:54 +0200

    More packages

--- development.sh
+++ development.sh
@@ -1,5 +1,6 @@
 
-sudo aptitude -y install automake cmake
+sudo aptitude -y install automake cmake build-essential checkinstall
 
 # opencv deps
 sudo aptitude -y install libavcodec-dev libswscale-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libjasper-dev libavformat-dev libunicap2-dev libunicapgtk2 libxine-dev intltool
+sudo aptitude install libjack-jackd2-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libsdl1.2-dev libtheora-dev libva-dev libvdpau-dev libvorbis-dev libx11-dev libxfixes-dev libxvidcore-dev texi2html yasm zlib1g-dev libtbb-dev libv4l-dev libopenexr-dev libunicap2-dev libavformat-dev libswscale-dev libdc1394-22-dev libgstreamer0.10-0 libgstreamer0.10-dev gstreamer0.10-tools gstreamer0.10-plugins-base libgstreamer-plugins-base0.10-dev gstreamer0.10-plugins-good gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad gstreamer0.10-ffmpeg python-numpy
