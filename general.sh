aeec2c7b476543b3da3000c0a6f6509ddc761cad
Joris <joris.gillis@mech.kuleuven.be>
2012-04-04 11:33:33 +0200

    install tree

--- general.sh
+++ general.sh
@@ -3,3 +3,4 @@ nautilus -q
 sudo aptitude -y install gitg
 sudo aptitude -y install subversion
 sudo aptitude -y install gstm emacs vim gimp winpdb doxygen meld nemiver samba ipe inkscape privoxy geogebra geogebra-gnome umlet kile scite kate openssh-server compizconfig-settings-manager dia pgf 
+sudo aptitude -y install gnome-tweak gnome-shell compiz tree
